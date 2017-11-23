79b536bd13ea405dbfa9fef3ab2df7b82a8e3f22
run2devtest <run2devtest@gmail.com>
2017-11-22 19:58:18 -0600

    pullled parsed cleaned data

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,32 @@
-import requests
-from bs4 import BeautifulSoup
+import pandas as pd
 
 
 URL = 'https://coinmarketcap.com/gainers-losers/'
+
+gainers_losers_dataframe = pd.read_html(URL)
+
+# gainers_1h = gainers_losers_dataframe[0]
+# gainers_7d = gainers_losers_dataframe[1]
+# gainers_24h = gainers_losers_dataframe[2]
+# losers_1h = gainers_losers_dataframe[3]
+# losers_7d = gainers_losers_dataframe[4]
+# losers_24h = gainers_losers_dataframe[5]
+
+
+def cleanStrings(stringObj):
+    return stringObj.replace('$', '').replace(',', '').replace('%', '').replace(' ', '')
+
+
+def cleanDataFrame(data_frame):
+    df = data_frame.copy()
+    df.columns = ['#', 'Name', 'Symbol', 'Volume', 'Price', 'Pct']
+
+    df['Name'] = df['Name'].astype(str)
+    df['Symbol'] = df['Symbol'].astype(str)
+    df['Volume'] = df['Volume'].apply(cleanStrings).astype(int)
+    df['Price'] = df['Price'].apply(cleanStrings).astype(float)
+    df['Pct'] = df['Pct'].apply(cleanStrings).astype(float)
+    return df
+
+for df in gainers_losers_dataframe:
+    print(cleanDataFrame(df))
