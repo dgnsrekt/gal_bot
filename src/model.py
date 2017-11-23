8618b757353b4231116bed273dea19c1d6605c3f
run2devtest <run2devtest@gmail.com>
2017-11-23 16:21:11 -0600

    added create/update to UserSettings model

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 from peewee import *
+from datetime import datetime
 
 db = PostgresqlDatabase('galbot')
 
@@ -10,7 +11,45 @@ class BaseModel(Model):
 
 
 class UserSettings(BaseModel):
-    chat_id = BigIntegerField()
+    chat_id = BigIntegerField(unique=True)
     filter_settings = CharField()
     create_date = DateTimeField()
     last_updated = DateTimeField()
+
+    def addUser(**kwargs):
+        try:
+            kwargs['last_updated'] = timeNow()
+            print(kwargs)
+            UserSettings.create(**kwargs)
+        except IntegrityError:
+            db.rollback()
+            UserSettings.updateUserSettings(**kwargs)
+
+    def updateUserSettings(**kwargs):
+        user_id_query = kwargs['chat_id']
+        query = UserSettings.update(filter_settings=kwargs[
+                                    'filter_settings'], last_updated=timeNow()).where(UserSettings.chat_id == user_id_query)
+        query.execute()
+
+
+def timeNow():
+    return datetime.now()
+
+
+def create_tables():
+    db.create_tables([UserSettings], True)
+
+
+UserSettings.addUser(chat_id='12345', filter_settings='Z',
+                     create_date=datetime.now())
+
+# UserSettings.updateUserSettings(chat_id='12345', filter_settings='G')
+# UserSettings.create(chat_id=1234565, filter_settings='N',
+#                     create_date=datetime.now(), last_updated=datetime.now())
+
+# print(UserSettings.select().where(
+#     UserSettings.chat_id == 1234565))
+
+# query = UserSettings.update(filter_settings='C', last_updated=datetime.now()).where(
+#     UserSettings.chat_id == 1234565)
+# query.execute()
