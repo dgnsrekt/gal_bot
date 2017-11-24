9e2f80134cd4b1940a48007fa24867e95ac8ef88
run2devtest <run2devtest@gmail.com>
2017-11-23 18:07:02 -0600

    fixed kwargs, updated logs

--- src/model.py
+++ src/model.py
@@ -1,5 +1,8 @@
 from peewee import *
 from datetime import datetime
+from logger import getLogger
+
+_logger = getLogger()
 
 db = PostgresqlDatabase('galbot')
 
@@ -18,18 +21,23 @@ class UserSettings(BaseModel):
 
     def addUser(**kwargs):
         try:
+            kwargs['create_date'] = timeNow()
             kwargs['last_updated'] = timeNow()
-            print(kwargs)
             UserSettings.create(**kwargs)
+            _logger.info(
+                'User {} added to the database.'.format(kwargs['chat_id']))
         except IntegrityError:
             db.rollback()
             UserSettings.updateUserSettings(**kwargs)
+            _logger.info(
+                'User {} already exists database updated.'.format(kwargs['chat_id']))
 
     def updateUserSettings(**kwargs):
         user_id_query = kwargs['chat_id']
         query = UserSettings.update(filter_settings=kwargs[
                                     'filter_settings'], last_updated=timeNow()).where(UserSettings.chat_id == user_id_query)
         query.execute()
+        _logger.info('User {} updated in database.'.format(kwargs['chat_id']))
 
 
 def timeNow():
