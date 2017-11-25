22e47abc32288653ccc6bf1c99ea77bfbb9761bd
run2devtest <run2devtest@gmail.com>
2017-11-25 16:15:23 -0600

    remove getdonate function.

--- src/gettoken.py
+++ src/gettoken.py
@@ -4,9 +4,3 @@ def GetToken():
     """
     with open('TOKEN', 'r') as TOKEN:
         return TOKEN.read().strip('\n').strip(' ')
-
-
-def GetDonateAddresses():
-    """Returns donations addresses."""
-    with open('DONATE', 'r') as DONATE:
-        return DONATE.read()
