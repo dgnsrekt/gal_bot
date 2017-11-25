32defdb7f2ffb4969adf2bcfaa0f3052c32e0e59
run2devtest <run2devtest@gmail.com>
2017-11-25 10:52:04 -0600

    added docstrings, GetDonateAddresses

--- src/gettoken.py
+++ src/gettoken.py
@@ -1,3 +1,12 @@
 def GetToken():
+    """Parses TOKEN file. Strips newlines and
+    whitespaces. Returns token string.
+    """
     with open('TOKEN', 'r') as TOKEN:
         return TOKEN.read().strip('\n').strip(' ')
+
+
+def GetDonateAddresses():
+    """Returns donations addresses."""
+    with open('DONATE', 'r') as DONATE:
+        return DONATE.read()
