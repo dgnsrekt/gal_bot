0472a5d9e15462fe4319f5265908a8abd340cee5
run2devtest <run2devtest@gmail.com>
2017-11-26 02:40:50 +0000

    added a function for getting pem/key

--- src/gettoken.py
+++ src/gettoken.py
@@ -1,6 +1,17 @@
+import os
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
 def GetToken():
     """Parses TOKEN file. Strips newlines and
     whitespaces. Returns token string.
     """
     with open('TOKEN', 'r') as TOKEN:
         return TOKEN.read().strip('\n').strip(' ')
+
+def GetPrivate():
+    return os.path.join(PATH, 'private.key')
+
+def GetCert():
+    return os.path.join(PATH, 'cert.pem')
+
